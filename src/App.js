60634d0aed13fb56b2cee33ddcb5675c731178f0
Nuha Maharoof <nuhamaharoof@Nuhas-MBP.fios-router.home>
2021-04-02 05:04:01 -0400

    created home page prototype

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Container, Row, Col, Button, Alert, Nav, Badge} from 'react-bootstrap'
+
 
 function App() {
   return (
     <div className="App">
+
+    <Nav variant="pills" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">How To Play</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
       <header className="App-header">
+        <container>
         <img src={logo} className="App-logo" alt="logo" />
+        <div className= "mb-2">
+          <h1>
+            Do You Know Me? 
+          </h1>
+          <h5><i>
+            Play with your friends!
+          </i>
+          </h5>
+        </div>
+ 
+        <div className="mb-2">
+          <Button variant="primary" size="lg" block> Create Game </Button>{' '}
+        </div>
+        <div className="mb-2">
+          <Button variant="primary" size="lg" block> Join Game </Button>{' '}
+        </div>
+
+
+
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          <h6>Team SOFT</h6>
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h6>Learn React</h6>
         </a>
+        </p>
+        </container>
       </header>
     </div>
   );
