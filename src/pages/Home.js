723ed9eb479f5ca098c2aa901ff196c5d3c1500d
Toocanzs <dupanzszs@gmail.com>
2021-05-03 14:49:33 -0400

    Add warning if using unsupported browser

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import logo from "../media/logo.svg";
 import {Button} from "react-bootstrap";
+import {isChrome, isFirefox, isSafari} from 'react-device-detect';
 
 export const Home = () => {
+    if(!(isChrome || isFirefox || isSafari)) {
+        alert("Warning: You are using an unsupported browser. This game is only tested with Chrome, Firefox, and Safari. You may encounter bugs because of this");
+    }
     return (
         <div className="Home">
             <header className="App-header">
