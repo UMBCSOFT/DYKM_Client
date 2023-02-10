e822ff3645ae8abee9abbab57d345f8d85237b20
Steven Laczko <stevendlaczko@gmail.com>
2023-02-10 14:06:36 -0500

    Updated CreateGame.js layout

--- src/pages/CreateGame.js
+++ src/pages/CreateGame.js
@@ -95,105 +95,101 @@ class CreateGame extends NetworkedPage {
         }
         else {
             return (
-                <div className="creategame App Window-page">
-                    <div className="mb-2 GameSettings">
+                <div className="App Window-page">
+                    <div className="GameSettings Center">
                         <h1 className="Center">CREATE GAME</h1>
-                        <div id="createGameSettings">
-                            <Form.Group as={Row} id="nickname">
-                                <Form.Label column sm={2}>
-                                    Nickname
-                                </Form.Label>
-                                <Col sm={10}>
-                                    <input type="text" placeholder="Enter a nickname!" value={this.state.name} onChange={this.handleNameChange}/>
-                                </Col>
-                            </Form.Group>
+                        <Form.Group as={Row} className="mb-4" id="nickname">
+                            <Form.Label column sm={3}>Nickname</Form.Label>
+                            <Col>
+                                <Form.Control type="text" placeholder="Enter a nickname!" value={this.state.name} onChange={this.handleNameChange}/>
+                            </Col>
+                        </Form.Group>
 
-                            <Form.Group as={Row} id="questionpack">
-                                <Form.Label column sm={2}>
-                                    Question Pack
-                                </Form.Label>
-                                <Col sm={10}>
-                                    <Tab.Container id="game-type" defaultActiveKey="#doyouknowme">
+                        <Form.Group as={Row} id="questionpack" className="mb-4">
+                            <Form.Label column sm={3}>
+                                Question Pack
+                            </Form.Label>
+                            <Col>
+                                <Tab.Container id="game-type" defaultActiveKey="#doyouknowme">
+                                    <Row>
+                                        <Col>
+                                            <ListGroup >
+                                                <ListGroup.Item
+                                                    action
+                                                    href="#doyouknowme"
+                                                    onClick={(e)=>this.onPackSelect(e)}
+                                                >
+                                                    Do You Know Me?
+                                                </ListGroup.Item>
+                                                <ListGroup.Item
+                                                    action
+                                                    href="#icebreakers"
+                                                    onClick={(e)=>this.onPackSelect(e)}
+                                                >
+                                                    Ice Breakers
+                                                </ListGroup.Item>
+                                            </ListGroup>
+                                        </Col>
                                         <Row>
-                                            <Col sm = "auto">
-                                                <ListGroup >
-                                                    <ListGroup.Item
-                                                        action
-                                                        href="#doyouknowme"
-                                                        onClick={(e)=>this.onPackSelect(e)}
-                                                    >
-                                                        Do You Know Me?
-                                                    </ListGroup.Item>
-                                                    <ListGroup.Item
-                                                        action
-                                                        href="#icebreakers"
-                                                        onClick={(e)=>this.onPackSelect(e)}
-                                                    >
-                                                        Ice Breakers
-                                                    </ListGroup.Item>
-                                                </ListGroup>
-                                            </Col>
-                                            <Col sm={8}>
-                                                <Tab.Content>
-                                                    <Tab.Pane eventKey="#doyouknowme">
-                                                        Official question pack - see if you <i>really</i> know your friends!
-                                                    </Tab.Pane>
-                                                    <Tab.Pane eventKey="#icebreakers">
-                                                        Great question pack to play with new friends!
-                                                    </Tab.Pane>
-                                                </Tab.Content>
-                                            </Col>
+                                            <Tab.Content>
+                                                <Tab.Pane eventKey="#doyouknowme">
+                                                    Official question pack - see if you <i>really</i> know your friends!
+                                                </Tab.Pane>
+                                                <Tab.Pane eventKey="#icebreakers">
+                                                    Great question pack to play with new friends!
+                                                </Tab.Pane>
+                                            </Tab.Content>
                                         </Row>
-                                    </Tab.Container>
-                                </Col>
-                            </Form.Group>
+                                    </Row>
+                                </Tab.Container>
+                            </Col>
+                        </Form.Group>
 
-                            <fieldset>
-                                <Form.Group as={Row}>
-                                    <Form.Label as="legend" column sm={2}>
-                                        Rounds
-                                    </Form.Label>
-                                    <Col sm={10}>
-                                        <Form.Check
-                                            type="radio"
-                                            label="1 Round"
-                                            defaultChecked={true}
-                                            onChange={(e)=>this.radioOnChange(e)}
-                                            name="formHorizontalRadios"
-                                            id="formHorizontalRadios1"
-                                            numvalue={1}
-                                        />
-                                        <Form.Check
-                                            type="radio"
-                                            label="5 Rounds"
-                                            onChange={(e)=>this.radioOnChange(e)}
-                                            name="formHorizontalRadios"
-                                            id="formHorizontalRadios2"
-                                            numvalue={5}
-                                        />
-                                        <Form.Check
-                                            type="radio"
-                                            label="10 Rounds"
-                                            onChange={(e)=>this.radioOnChange(e)}
-                                            name="formHorizontalRadios"
-                                            id="formHorizontalRadios3"
-                                            numvalue={10}
-                                        />
-                                    </Col>
-                                </Form.Group>
-                            </fieldset>
+                        <Form.Group as={Row} className="mb-4">
+                            <Form.Label column sm={3}>
+                                Rounds
+                            </Form.Label>
+                            <Col className="text-left">
+                                <Form.Check
+                                    type="radio"
+                                    label="1 Round"
+                                    defaultChecked={true}
+                                    onChange={(e)=>this.radioOnChange(e)}
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios1"
+                                    numvalue={1}
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="5 Rounds"
+                                    onChange={(e)=>this.radioOnChange(e)}
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios2"
+                                    numvalue={5}
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="10 Rounds"
+                                    onChange={(e)=>this.radioOnChange(e)}
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios3"
+                                    numvalue={10}
+                                />
+                            </Col>
+                        </Form.Group>
 
-                            <Form.Group controlId="roomcode">
-                                <Form.Label>Secret Code</Form.Label>
+                        <Form.Group controlId="roomcode">
+                            <Form.Label column sm={3}>Secret Code</Form.Label>
+                            <Col>
                                 <Form.Text><b>{this.state.roomCode}</b></Form.Text>
-                            </Form.Group>
+                            </Col>
+                        </Form.Group>
 
-                            <Form.Group as={Row}>
-                                <Col sm={{ span: 10, offset: 2 }}>
-                                    <Button variant="primary" type="submit" onClick={() => this.CreateRoom()}>Create the game!</Button>
-                                </Col>
-                            </Form.Group>
-                        </div>
+                        <Form.Group as={Row}>
+                            <Col sm={{ span: 10, offset: 2 }}>
+                                <Button variant="primary" type="submit" onClick={() => this.CreateRoom()}>Create the game!</Button>
+                            </Col>
+                        </Form.Group>
                     </div>
                 </div>
 
